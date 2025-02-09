b587d9c03e436ea739e4941ec4b29daaa704c616
Guilherme Antonio Silva <guiantoni596@gmail.com>
2025-02-09 18:00:31 -0300

    Criação de Packages

---
+++ src/main/java/br/com/gas/ApiRestContatos/ApiRestContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.gas.ApiRestContatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestContatosApplication.class, args);
+	}
+
+}
