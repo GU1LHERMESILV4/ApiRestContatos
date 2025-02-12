b42203ecf31b419a5a00ed8c70aba22618371513
Guilherme Antonio Silva <guiantoni596@gmail.com>
2025-02-11 21:15:03 -0300

    Criação do ContatoController

--- src/main/java/br/com/gas/ApiRestContatos/controller/ContatoController.java
+++ src/main/java/br/com/gas/ApiRestContatos/controller/ContatoController.java
@@ -1,66 +1,35 @@
 package br.com.gas.ApiRestContatos.controller;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.gas.ApiRestContatos.model.Contato;
 import br.com.gas.ApiRestContatos.service.ContatoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/contatos/") //http://localhost:8080/api/contatos/
+@RequestMapping("/api/contatos")
 public class ContatoController {
 
-    @Autowired
-    private ContatoService contatoService;
+    private final ContatoService contatoService;
+
+    public ContatoController(ContatoService contatoService) {
+        this.contatoService = contatoService;
+    }
 
     @PostMapping
-    public ResponseEntity<Contato> save(@RequestBody Contato contato){
-        Contato newContato = contatoService.save(contato);
-        if(newContato == null)
-            return ResponseEntity.badRequest().build(); //400
-        return ResponseEntity.ok(newContato);
+    public ResponseEntity<Contato> criarContato(@RequestBody Contato contato) {
+        return ResponseEntity.ok(contatoService.salvar(contato));
     }
 
-    @GetMapping("/api/contatos/{id}")
-    public ResponseEntity<Optional<Contato>> findById(@PathVariable Long id){
-        Optional<Contato> findContato = contatoService.findbyId(id);
-        if(findContato == null)
-            return ResponseEntity.badRequest().build(); //400
-        return ResponseEntity.ok(findContato); //200
+    @GetMapping("/pessoa/{pessoaId}")
+    public ResponseEntity<List<Contato>> listarContatosPorPessoa(@PathVariable Long pessoaId) {
+        return ResponseEntity.ok(contatoService.listarPorPessoa(pessoaId));
     }
 
-    @GetMapping
-    public ResponseEntity<List<Contato>> findAll(){
-        List<Contato> findContato = contatoService.findAll();
-        if(findContato == null)
-            return ResponseEntity.badRequest().build(); //400
-        return ResponseEntity.ok(findContato); //200
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarContato(@PathVariable Long id) {
+        contatoService.deletar(id);
+        return ResponseEntity.noContent().build();
     }
-
-    @PutMapping
-    public ResponseEntity<Contato> update(@RequestBody Contato contato){
-        Contato updContato = contatoService.update(contato);
-        if(updContato == null)
-            return ResponseEntity.badRequest().build(); //400
-        return ResponseEntity.ok(updContato); //200
-    }
-
-    @DeleteMapping("/api/contatos/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id){
-        contatoService.delete(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);//204
-    }
-
 }
